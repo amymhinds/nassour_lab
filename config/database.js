2f39e7ab760ba0cd0994ebcc4e178d865fcdb21d
Amy Hinds <amyhinds@Amys-MacBook-Air.local>
2023-03-01 10:56:10 -0800

    commiting files

---
+++ config/database.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+
+const db = process.env.MONGOURI;
+
+const connectDB = async () => {
+  try {
+    console.log(db);
+    await mongoose.connect(`${db}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
