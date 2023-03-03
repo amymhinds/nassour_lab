79451b6f7a80d9934d22011f34d051e709243125
Amy Hinds <amyhinds@Amys-MacBook-Air.local>
2023-03-03 09:24:27 -0800

    research page

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ require("./config/database");
 
 var indexRouter = require("./routes/index");
 var teamRouter = require("./routes/team");
+var researchRouter = require("./routes/research");
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/team", teamRouter);
+app.use("/research", researchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
